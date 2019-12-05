c155afd03a9e9c85fb918d04bee3d8e428f250e5
Eduardo Domingos <epcdomingos@gmail.com>
2019-12-05 14:04:51 +0000

    WIP

--- front-page.php
+++ front-page.php
@@ -510,6 +510,39 @@ get_header();
 					</div>
 				</div>
 			</section>
+		<?php elseif( get_row_layout() == 'communication' ): ?>
+			<?php 
+				$settings = get_sub_field('settings');
+				$contents = get_sub_field('communications');
+			?>
+			<section class="block">
+            <div class="container">
+				<h2><?php echo $settings['title'] ?></h2>
+				<div class="row">
+					<?php foreach($contents as $content): ?>
+						<?php foreach($content as $communication): ?>
+						<div class="col-sm-12 col-md-6 col-lg-3">
+							<article class="teaser">
+								<?php if($communication['url']): ?>
+									<a href="<?php echo $communication['url']; ?>">
+								<?php endif;?>
+								<div class="media">
+									<?php echo wp_get_attachment_image($communication['photo'], 'large'); ?>
+								</div>
+								<div class="text-details">
+									<h1><?php echo $communication['title'];?></h1>
+									<p class="meta"><?php echo $communication['date']; ?></p>
+								</div>
+								<?php if($communication['url']): ?>
+									</a>
+								<?php endif;?>
+							</article>
+						</div>
+						<?php endforeach; ?>
+					<?php endforeach; ?>
+				</div>
+			</div>
+			</section>
 		<?php endif; ?>
 		
     <?php endwhile; ?>
