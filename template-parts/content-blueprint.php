42bca07e7fdf13715d399f4bb0b2631ab779b3b9
Eduardo Domingos <epcdomingos@gmail.com>
2019-12-26 12:30:39 +0000

    Style fixings

--- template-parts/content-blueprint.php
+++ template-parts/content-blueprint.php
@@ -93,9 +93,14 @@
                         ?>
                         <?php foreach($blueprints as $key=>$item) :?>
                             <div class="c-modal" id="modal-<?php echo $key + 1; ?>">
-                                <button class="modal-close">Fechar</button>
+                                <button class="modal-close">
+                                    <span class="screen-reader-text">Fechar</span>
+                                    <svg width="21" height="25" viewBox="0 0 21 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M20.1502 25L16.6152 25L10.2102 14.745H10.1402L3.73516 25H0.375158L8.35516 12.575L0.585158 0.57H4.12016L10.2452 10.3H10.3152L16.5802 0.57H19.9402L12.1002 12.47L20.1502 25Z"/>
+                                    </svg>
+                                </button>
                                 <div class="modal-inner">
-                                    <figure class="media">
+                                    <figure class="media main-modal-media">
                                         <?php echo wp_get_attachment_image($item['blueprint']['image'], 'large'); ?>
                                     </figure>
                                     <div class="content-wrapper">
--- template-parts/content-timeline.php
+++ template-parts/content-timeline.php
@@ -18,7 +18,11 @@
                         <div class="modals">
                             <?php foreach($events as $event):?>
                             <div class="c-modal" id="modal-<?php echo $event['event']['year']; ?>">
-                                <button class="modal-close">Fechar</button>
+                                <button class="modal-close"><span class="screen-reader-text">Fechar</span>
+                                <svg width="21" height="25" viewBox="0 0 21 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M20.1502 25L16.6152 25L10.2102 14.745H10.1402L3.73516 25H0.375158L8.35516 12.575L0.585158 0.57H4.12016L10.2452 10.3H10.3152L16.5802 0.57H19.9402L12.1002 12.47L20.1502 25Z"/>
+                                </svg>
+                            </button>
                                 <div class="modal-inner">
                                 
                                     <?php if(!empty($event['event']['photos'])): ?>
@@ -31,7 +35,7 @@
                                                 <?php endforeach;?>
                                             </ul>
                                         <?php else:?>
-                                            <figure class="media">
+                                            <figure class="media main-modal-media">
                                                 <?php echo wp_get_attachment_image($event['event']['photos'][0]['photo'], 'modal-gallery'); ?>
                                             </figure>
                                         <?php endif;?>
